7558dd43eac2792bb40917790b16d546201d43e9
L Gabriel Miller <gmiller2007@gmail.com>
2014-05-11 17:57:34 -0400

    Adds pagerank methods CalculatePagerank, AddNode, and register. Adds node struct and node method rank.

--- pagerank.go
+++ pagerank.go
@@ -1,31 +1,91 @@
-package main
+package pagerank
 
-type pageRank struct {
-    //id          int
-    //links_out   int
-    //pagerank    int
-    ids         []int{}
-    links       [][]int{}
-    pageranks   []int{}
+const (
+    DF float32 0.85
+)
+
+type PageRank struct {
+    Nodes       map[int32]Node{}
+    ids         map[string]int32
 }
 
-func New() *pageRank {
-    pagerank := new(pageRank)
-    return pagerank
+type Node struct {
+    out         map[string]int64
+    in          map[string]int64
+    pagerank    map[int32]float32
 }
 
-func AddNode(node int) *pageRank {
-    currentIds = pageRank.ids
-    //pageRank.ids = currentIds + node
+func New() *Pagerank {
+    return new(Pagerank)
 }
 
-func AddLink(originLink int, destinationLink int) *pageRank {
-    links = pageRank.links
-    //pageRank.links[originLink][destinationLink] = true
+func (p *Pagerank) CalculatePagerank(iterations int32) {
+
+    // create inbound id lookups for each node
+    for _, node := range Nodes {
+        for _, id := range node.out {
+            _, exists := p.Nodes[id]
+            if exists {
+                append(id, p.Nodes[id].in)
+            }
+        }
+    }
+
+    for i in range iterations {
+        for key, node := range Nodes {
+            // calculate pagerank for individual node
+            node.rank(i, &p)
+        }
+    }
 }
 
-func CalculateSingle(float64, int, int) *pageRank {
+// Add a node to the pagerank graph
+func (p *Pagerank) AddNode(id string, links map[string]string) {
+
+    // register this node's id
+    identifier := p.register(id)
+
+    out := []int32{}
+    in := []int32{}
+
+    // register an id for each linked node
+    for key, id := range links {
+        append(out, p.register(id))
+    }
+
+    rankmap := make(map[int32]float32)
+    rankmap[0] = 1 // At the 0th iteration each node has a rank of 1
+
+    node := Node{
+        out,
+        in,
+        rankmap
+    }
+
+    p.Nodes[identifier] = node
 }
 
-func CalculateFull(float64, int, int) *pageRank {
+// Return the identifying int for a string within p.ids
+func (p *Pagerank) register(id string) (identifier int){
+
+    // if id exists in p.ids, use the existing integer for the identifier
+    identifer, exists := p.ids[id]
+    if exists {
+        return identifier
+    }
+
+    // if id does not exist in p.ids, map the id to len(ids)+1 and use that
+    newid := len(p.ids) + 1
+    p.ids[id] = p.ids[newid]
+    return newid
+}
+
+// calculate the rank for a single node
+func (n *Node) rank(iteration, *p Pagerank) float32 {
+    i := iteration - 1
+    sums := 0.0
+    for id := range n.in {
+        sums += p.Nodes[id].pagerank[i] / len(p.Nodes[id].in)
+    }
+    return (1 - DF) + (DF * sums)
 }
